14df373f6d117939dd91aa35be3dd6da515f3a14
Alex Diego <66486335+alexdiegoo@users.noreply.github.com>
2023-05-24 08:20:00 -0300

    efeito de zoom na camera

---
+++ Salve a Floresta/Assets/Scripts/Utils/CameraZoom.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Rigidbody2D playerRb;
+
+    bool zoomIn;
+
+    [Range(2, 10)]
+    [SerializeField] float zoomSize;
+
+    [Range(0.01f, 0.1f)]
+    [SerializeField] float zoomSpeed;
+
+    [Range(1, 3)]
+    [SerializeField] float waitTime;
+
+    float waitCounter;
+
+    void ZoomIn()
+    {
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomSize, zoomSpeed);
+
+    }
+
+    void ZoomOut()
+    {
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 10, zoomSpeed);
+    }
+
+    private void LateUpdate()
+    {
+        if(Mathf.Abs(playerRb.velocity.magnitude) < 8)
+        {
+            waitCounter += Time.deltaTime;
+            if(waitCounter > waitTime)
+            {
+                zoomIn = true;
+            }
+        }
+        else{
+            zoomIn = false;
+            waitCounter = 0;
+        }
+
+        if(zoomIn)
+        {
+            ZoomIn();
+        }
+        else
+        {
+            ZoomOut();
+        }
+    }
+}
