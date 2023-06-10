2bae6b91a8d366ac0a15fd981d12f7290f858a5b
Alex Diego <66486335+alexdiegoo@users.noreply.github.com>
2023-06-10 10:26:55 -0300

    efeito de fade ao carregar cena

--- Salve a Floresta/Assets/Scripts/GameOverManager.cs
+++ Salve a Floresta/Assets/Scripts/GameOverManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
     private int previousSceneIndex;
+    public Image black;
+    public Animator anim;
 
     private void Start()
     {
@@ -14,6 +17,13 @@ public class GameOverManager : MonoBehaviour
 
     public void RestartGame()
     {
+        StartCoroutine(Fading());
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a==1);
         SceneManager.LoadScene(previousSceneIndex);
     }
 }
--- Salve a Floresta/Assets/Scripts/MainMenuManager.cs
+++ Salve a Floresta/Assets/Scripts/MainMenuManager.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private string nextScene;
+
+    public Image black;
+    public Animator anim;
+
     public void Play()
     {
-        SceneManager.LoadScene(nextScene);
+        StartCoroutine(Fading());
     }
     
     public void Quit()
     {
         Debug.Log("Saiu do jogo");
         Application.Quit();
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a==1);
+        SceneManager.LoadScene(nextScene);
     }
 }
