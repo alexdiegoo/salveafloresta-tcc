5b0a02f67c2e50bda69e75fd2a8902a352967fc6
Alex Diego <66486335+alexdiegoo@users.noreply.github.com>
2023-06-11 00:21:15 -0300

    magic portion

--- Salve a Floresta/Assets/Scripts/Cuca/MagicPortionController.cs
+++ Salve a Floresta/Assets/Scripts/Cuca/MagicPortionController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MagicPortionController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float timeDestroy = 2f;
+
     void Start()
     {
-        
+        Destroy(gameObject, timeDestroy);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
